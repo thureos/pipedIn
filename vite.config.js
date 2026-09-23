7bc21701aa6285ab0d033bfbcd46afb8e4fded3d
Luis Barron <luis84barron@gmail.com>
2026-09-22 21:46:35 -0500

    live code on local, full view, minimized columns

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({ plugins: [vue(), tailwindcss()] })
+export default defineConfig({ plugins: [vue(), tailwindcss()], server: { watch: { usePolling: true } } })
